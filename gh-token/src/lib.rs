33faa641c48fd714e210bcc01a73fdee09e32d1d
David Tolnay <dtolnay@gmail.com>
2023-03-05 15:52:00 -0800

    Release gh-token 0.1.1

--- gh-token/src/lib.rs
+++ gh-token/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/gh-token/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/gh-token/0.1.1")]
 #![allow(clippy::module_name_repetitions, clippy::missing_errors_doc)]
 
 use crate::error::ParseError;
